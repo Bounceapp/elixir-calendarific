3620a35112658e125e481d69bf004857ed2c9a15
Luís Jesus <luisjesus89@gmail.com>
2021-03-16 14:28:43 +0000

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Calendarific.MixProject do
   def project do
     [
       app: :calendarific,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: "A wrapper for the holiday API Calendarific",
